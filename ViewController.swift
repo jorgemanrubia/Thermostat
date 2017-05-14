df521c99529d2fa85f5c1781e93d8437a3eefffc
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-05-14 11:18:25 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Thermostat
+//
+//  Created by JORGE MANRUBIA DIEZ on 14/05/2017.
+//  Copyright © 2017 JORGE MANRUBIA DIEZ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    var temperature = 68
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
